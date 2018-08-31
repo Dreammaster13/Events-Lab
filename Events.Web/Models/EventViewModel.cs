fa59907b98ea522d45d3a6e06aa1503f3b75b8c1
Rim Fardiyev <threeacesstl@gmail.com>
2018-08-31 00:56:27 -0500

    HomeController optimized

--- Events.Web/Models/EventViewModel.cs
+++ Events.Web/Models/EventViewModel.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Events.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web;
 
 namespace Events.Web.Models
@@ -18,5 +20,21 @@ namespace Events.Web.Models
         public string Author { get; set; }
 
         public string  Location { get; set; }
+
+        public static Expression<Func<Event, EventViewModel>> ViewModel
+        {
+            get
+            {
+                return e => new EventViewModel()
+                {
+                    Id = e.Id,
+                    Title = e.Title,
+                    StartDateTime = e.StartDateTime,
+                    Duration = e.Duration,
+                    Author = e.Author.FullName,
+                    Location = e.Location
+                };
+            }
+        }
     }
 }
